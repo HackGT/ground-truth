3ccec2643a9b8e311866231a5603f927d76bbb1e
Ryan Petschek <petschekr@gmail.com>
2019-08-06 21:58:59 -0400

    Add contact email to template footer

--- src/templates.ts
+++ src/templates.ts
@@ -53,12 +53,18 @@ export class Template<T extends TemplateContent> {
 		this.template = Handlebars.compile(data);
 	}
 
-	public render(input: T): string {
+	public render(input: Partial<T>): string {
 		if (!config.server.isProduction) {
 			Handlebars.registerPartial("main", fs.readFileSync(path.resolve("src/ui", "partials", "main.hbs"), "utf8"));
 			this.loadTemplate();
 		}
-		return this.template!(input);
+		const renderData = {
+			siteTitle: config.server.name,
+			contactEmail: config.email.contactAddress,
+			includeJS: null,
+			...input
+		} as T;
+		return this.template!(renderData);
 	}
 }
 
@@ -91,9 +97,7 @@ uiRoutes.route("/").get(authenticateWithRedirect, async (request, response) => {
 		}
 	}
 	let templateData = {
-		siteTitle: config.server.name,
 		title: "Home",
-		includeJS: null,
 
 		user: request.user,
 		loginMethod: await bestLoginMethod(request.user.email),
@@ -107,7 +111,6 @@ uiRoutes.route("/login").get(async (request, response) => {
 		return;
 	}
 	let templateData = {
-		siteTitle: config.server.name,
 		title: "Log in",
 		includeJS: "login",
 
@@ -122,9 +125,7 @@ uiRoutes.route("/login").get(async (request, response) => {
 
 uiRoutes.route("/login/forgot").get((request, response) => {
 	let templateData = {
-		siteTitle: config.server.name,
 		title: "Forgot Password",
-		includeJS: null,
 
 		error: request.flash("error"),
 		success: request.flash("success")
@@ -148,9 +149,7 @@ uiRoutes.route("/login/forgot/:code").get(async (request, response) => {
 		return;
 	}
 	let templateData = {
-		siteTitle: config.server.name,
 		title: "Reset Password",
-		includeJS: null,
 
 		error: request.flash("error"),
 		success: request.flash("success"),
@@ -165,9 +164,7 @@ uiRoutes.route("/login/changepassword").get(authenticateWithRedirect, async (req
 		return;
 	}
 	let templateData = {
-		siteTitle: config.server.name,
 		title: "Change Password",
-		includeJS: null,
 
 		error: request.flash("error"),
 		success: request.flash("success")
@@ -177,7 +174,6 @@ uiRoutes.route("/login/changepassword").get(authenticateWithRedirect, async (req
 
 uiRoutes.route("/admin").get(isAdmin, async (request, response) => {
 	let templateData = {
-		siteTitle: config.server.name,
 		title: "Admin",
 		includeJS: "admin",
 
