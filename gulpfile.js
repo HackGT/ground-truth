70dc9f1e4aeb26eea662ddceeac08c02ef4f35c9
Ayush Goyal <agoyal2001@gmail.com>
2020-11-13 16:53:57 -0500

    Move bugsnag reporting to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ const gulp = require("gulp");
 const ts = require('gulp-typescript');
 const babel = require('gulp-babel');
 const del = require('del');
+const gulpif = require('gulp-if');
+const { reportBuild } = require('gulp-bugsnag');
 
+const VERSION_NUMBER = JSON.parse(fs.readFileSync(path.resolve(__dirname, "package.json"), "utf8")).version;
 const tsProject = ts.createProject('./tsconfig.json');
 
 // Clean dist folder
-gulp.task('clean', function () {
+gulp.task('clean', () => {
     return del('dist');
 });
 
@@ -16,13 +19,20 @@ gulp.task('build:js', () => {
         .pipe(babel({
             presets: ['@babel/env']
         }))
-        .pipe(gulp.dest('dist/static/js'))
+        .pipe(gulp.dest('dist/static/js'));
 });
 
+// If production environment and bugsnag api key, report build to bugsnag
 gulp.task('build:ts', () => {
     return tsProject.src()
         .pipe(tsProject())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(gulpif(process.env.NODE_ENV == "production" && process.env.BUGSNAG, reportBuild({
+            apiKey: process.env.BUGSNAG,
+            appVersion: VERSION_NUMBER,
+            releaseStage: "production",
+            autoAssignRelease: true
+        })));
 });
 
 gulp.task('build', gulp.parallel(
@@ -31,17 +41,17 @@ gulp.task('build', gulp.parallel(
 ));
 
 // Copy files
-gulp.task('copy:css', function () {
+gulp.task('copy:css', () => {
     return gulp.src('src/static/css/**')
         .pipe(gulp.dest('dist/static/css'));
 });
 
-gulp.task('copy:templates', function () {
+gulp.task('copy:templates', () => {
     return gulp.src('src/templates/**')
         .pipe(gulp.dest('dist/templates'));
 });
 
-gulp.task('copy:emails', function () {
+gulp.task('copy:emails', () => {
     return gulp.src('src/emails/**')
         .pipe(gulp.dest('dist/email'));
 });
